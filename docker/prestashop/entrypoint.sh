4cd0847cf8e2d6ea81690374e86a0be2f7ab1e19
Damian <damian@d0niek.pl>
2021-11-04 09:18:57 +0100

    fixup! Add supervisor to run cron jobs

--- docker/prestashop/entrypoint.sh
+++ docker/prestashop/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$1" = "/tmp/docker_run.sh" ]; then
+if [ "$1" = "/usr/bin/supervisord" ]; then
 	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
 
 	if [ $PS_DEV_MODE -eq 1 ]; then
