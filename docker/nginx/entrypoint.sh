5837386ed862d982e48fac60a028bafbd22334c5
Damian <damian@d0niek.pl>
2021-11-12 15:16:53 +0100

    Set server_name in nginx config
    Some modules (ioncube) required this field to be set

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "nginx" ]; then
     echo "Create default.conf"
 
-    envsubst '${PS_FOLDER_ADMIN}' \
+    envsubst '${PS_FOLDER_ADMIN} ${PS_DOMAIN}' \
         < /etc/nginx/conf.d/default.conf.template \
         > /etc/nginx/conf.d/default.conf
 fi
