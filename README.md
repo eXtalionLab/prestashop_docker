dd2e27d0fde1d35d55a3d5ff44aea2522fa19624
Damian Glinkowski <damian@d0niek.pl>
2022-04-04 16:25:15 +0200

    Update cron jobs setup

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ file **cronjobs/jobs.dist**:
 
 ```
 cp cronjobs/jobs.dist cronjobs/jobs
-echo "* * * * * /cronjobs/custom_script.sh" > cronjobs/jobs
-echo "* 8 * * * /cronjobs/custom_script_2.sh" >> cronjobs/jobs
+echo "* * * * * /cronjobs/custom-script.sh" > cronjobs/jobs
+echo "* 8 * * * /cronjobs/custom-script-2.sh" >> cronjobs/jobs
 ```
 
 Add custom scripts to **cronjobs/** directory. Remember to add **.sh** extension
 to files. Entrypoint will add `x` mode to all files **cronjobs/\*.sh**.
+
+**Note!** Name custom scripts like `modulename-cronaction.sh`.
--- cronjobs/example.sh
+++ cronjobs/example.sh
@@ -6,6 +6,7 @@ root_path=$(dirname "$0")
 source "${root_path}/helper"
 
 script_name=$(get_script_name "$0")
+log_file="$(get_log_file "${root_path}" "${script_name}")"
 
 # Uncomment if you don't want to run job on localhost
 #dont_run_on_localhost
@@ -14,15 +15,26 @@ script_name=$(get_script_name "$0")
 # See https://cronitor.io
 #run_cronitor "$script_name"
 
-schema=$(get_schema)
+# Your cron job
+# use http
 domain=$(get_domain)
 admin_folder=$(get_admin_folder)
 
-# Your cron job
-token="CHANGE_ME!"
+#echo "" >> "${log_file}"
+#info "Rebuild search index" >> "${log_file}"
+#curl -Ls \
+#    "${domain}/${admin_folder}/index.php?controller=AdminSearch&action=searchCron&ajax=1&full=1&token=CHANGE_ME&id_shop=1" \
+#    >> "${log_file}" 2>&1
 
-echo "Rebuild search index"
-#curl -Ls "${schema}://${domain}/${admin_folder}/index.php?controller=AdminSearch&action=searchCron&ajax=1&full=1&token=${token}&id_shop=1"
+# or cli
+module_dir="$(get_module_dir "$script_name")" # /var/www/html/modules/modulename
+admin_dir="$(get_admin_dir)"                  # /var/www/html/adminxyz
+
+#echo "" >> "${log_file}"
+#info "Update currency rates" >> "${log_file}"
+#php ${admin_dir}/cron_currency_rates.php \
+#    secure_key=CHANGE_ME \
+#    >> "${log_file}" 2>&1
 # END Your cron job
 
 # Uncomment to monitor job with cronitor
