c0432abc00561d49a776a7ceff43cfce9edad987
Damian <damian@d0niek.pl>
2023-06-12 19:09:50 +0200

    feat: update nginx config
    See https://devdocs.prestashop-project.org/8/basics/installation/nginx/

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "nginx" ]; then
     echo "Create default.conf"
 
-    envsubst '${PS_FOLDER_ADMIN} ${PS_DOMAIN}' \
+    envsubst '${CLIENT_MAX_BODY_SIZE} ${PS_FOLDER_ADMIN} ${PS_DOMAIN}' \
         < /etc/nginx/conf.d/default.conf.template \
         > /etc/nginx/conf.d/default.conf
 fi
